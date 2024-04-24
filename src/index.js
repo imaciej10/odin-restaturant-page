fe46c5225177ba5947d59ddee5ec2a54688afae2
imaciej10 <160747504+imaciej10@users.noreply.github.com>
2024-04-24 18:25:48 +0200

    Add styling and basic js interaction

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ import Chef5 from "./chef5.png";
 import fbLogo from "./fb.png";
 import linkedLogo from "./linked.png";
 import igLogo from "./ig.png";
-import map from "./map.jpeg";
-import map2 from "./map2.png";
-import gallery1 from "./meal1.jpeg";
+import map from "./map.jpg";
+import map2 from "./map2.jpg";
+import gallery1 from "./meal1.jpg";
 import gallery2 from "./meal2.jpg";
-import gallery3 from "./kitchen1.jpeg";
-import gallery4 from "./kitchen2.jpeg";
+import gallery3 from "./kitchen1.jpg";
+import gallery4 from "./kitchen2.jpg";
 
 const socialContainer = document.createElement("div");
 const socialTitle = document.createElement("div");
@@ -29,17 +29,24 @@ const aboutBtn = document.querySelector(".about");
 const galleryBtn = document.querySelector(".gallery");
 const content = document.getElementById("content");
 
+const buttons = [menuBtn, aboutBtn, galleryBtn, homeBtn];
+function resetBtnClasses() {
+  buttons.forEach((button) => button.classList.remove("active"));
+}
+
 function setMediaBar() {
   const logoArr = [ig, fb, linkedIn];
   linkedIn.src = linkedLogo;
   ig.src = igLogo;
   fb.src = fbLogo;
   socialTitle.textContent = "Find us on social media";
-  footer.classList.add("footer");
-  socialContainer.classList.add("social");
-  socialContainer.appendChild(socialTitle);
+
+  setClass(footer, "footer");
+  setClass(socialTitle, "socialTitle");
+  setClass(socialContainer, "social");
+
   logoArr.forEach((logo) => {
-    logo.classList.add("logo");
+    setClass(logo, "logo");
     socialContainer.appendChild(logo);
   });
 }
@@ -59,7 +66,7 @@ const loadPage = function () {
   const heading = document.createElement("h1");
   const cooks = document.createElement("div");
   const cooksTitle = document.createElement("div");
-  cooksTitle.textContent = "Our amazing cooks";
+  cooksTitle.textContent = "Give warm welcome to our amazing cooks";
 
   const names = [];
   const containers = [];
@@ -89,7 +96,7 @@ const loadPage = function () {
     containers.push(container);
   }
 
-  names[0].textContent = "Martin";
+  names[0].textContent = "Martina";
   names[1].textContent = "Arthur";
   names[2].textContent = "Palmer";
   names[3].textContent = "Dutch";
@@ -102,7 +109,7 @@ const loadPage = function () {
   setClass(cook4, "avi");
   setClass(cook5, "avi");
   setClass(cooks, "cooks");
-
+  setClass(cooksTitle, "cooksTitle");
   containers.forEach((container, index) => {
     container.appendChild(names[index]);
     container.appendChild([cook1, cook2, cook3, cook4, cook5][index]);
@@ -120,6 +127,7 @@ const loadPage = function () {
     "At Restaurant Tiny Bites, crafting our meals is a meticulous art form, infused with passion and creativity. We begin by sourcing only the freshest, finest ingredients from local farmers and trusted suppliers, ensuring each component contributes to a symphony of flavors. Our chefs, skilled artisans of the culinary realm, embark on a journey of experimentation and innovation, constantly pushing boundaries to create dishes that tantalize the taste buds and ignite the senses. Every recipe is a labor of love, meticulously crafted with attention to detail and an unwavering commitment to quality. From delicate appetizers to hearty mains and decadent desserts, each dish tells a story, a narrative of tradition, innovation, and gastronomic delight. We strive to cater to diverse palates and dietary preferences, offering a wide array of options that cater to vegetarians, vegans, and meat lovers alike. Presentation is paramount at Tiny Bites, with each plate meticulously arranged to captivate not only the palate but also the eye. Our dedication to excellence extends beyond the kitchen, with attentive staff providing impeccable service, ensuring every dining experience is memorable and extraordinary. At Tiny Bites, we believe that a meal is not just sustenance; it is an experience to be savored, shared, and cherished. Join us on a culinary adventure where every bite is a journey into the sublime, where flavors dance on the palate and memories are made to last a lifetime.";
   content.append(heading, p1, p2, p3, cooksTitle, cooks);
 
+  body.append(socialTitle);
   body.append(socialContainer);
   body.append(footer);
 };
@@ -138,10 +146,15 @@ function createMealElement(elem) {
   priceElement.textContent = elem.price;
   descriptionElement.textContent = elem.description;
 
-  mealElement.appendChild(nameElement);
-  mealElement.appendChild(priceElement);
-  mealElement.appendChild(descriptionElement);
+  setClass(nameElement, "mealName");
+  setClass(priceElement, "mealPrice");
+  setClass(descriptionElement, "mealDescription");
 
+  mealElement.appendChild(nameElement);
+  mealElement.appendChild(descriptionElement);
+  mealElement.appendChild(priceElement);
+
+  setClass(mealElement, "mealContainer");
   return mealElement;
 }
 const loadMenu = function () {
@@ -150,9 +163,9 @@ const loadMenu = function () {
   setMediaBar();
   setFooter();
   const menuContainer = document.createElement("div");
-  const mealContainer = document.createElement("div");
-  const menuTitle = document.createElement("menu-title");
+  const menuTitle = document.createElement("div");
 
+  setClass(menuContainer, "menuContainer");
   function Meal(name, price, description) {
     (this.name = name), (this.price = price), (this.description = description);
   }
@@ -189,14 +202,16 @@ const loadMenu = function () {
 
   const meals = [meal1, meal2, meal3, meal4, meal5];
 
-  menuTitle.textContent = "Our mouth-watering meals that await for you: ";
+  menuTitle.textContent = "Our mouth-watering meals that await for you";
+
+  setClass(menuTitle, "menuTitle");
 
   meals.forEach((meal) => {
     menuContainer.appendChild(createMealElement(meal));
   });
 
-  menuContainer.appendChild(menuTitle, mealContainer);
-  content.appendChild(menuContainer, socialContainer, footer);
+  content.appendChild(menuTitle);
+  content.appendChild(menuContainer);
 };
 
 const loadAbout = function () {
@@ -211,7 +226,7 @@ const loadAbout = function () {
 
   mapImage.src = map;
   mapImage2.src = map2;
-  aboutTitle.textContent = "Tiny Bites is here and boy it's here for good!";
+  aboutTitle.textContent = "Tiny Tastes is here and boy it's here for good!";
   aboutText.textContent =
     "Welcome to the quirky culinary cosmos of Tiny Bites, where our kitchen is a playground and flavors are the merry-go-round! Prepare your taste buds for a rollercoaster ride through a universe of minuscule munchies and microscopic marvels. Our chefs are whimsical wizards, concocting dishes that defy gravity and tickle your funny bone. Ever tried a pancake the size of a thumbprint or a burger smaller than a button? At Tiny Bites, we believe in thinking outside the breadbox and playing with our food (with parental supervision, of course!). Every meal is a mini adventure, a treasure hunt for your taste buds in a world where big flavors come in pint-sized portions. So grab your magnifying glass and join us on a gastronomic journey where the only rule is: the tinier, the tastier! Because at Tiny Bites, we don't just serve meals; we serve smiles, giggles, and a pinch of pixie dust. Let's shrink down and chow down together at the wackiest restaurant this side of the teacup!";
   location.textContent = "You can find us here: ";
@@ -267,6 +282,7 @@ const loadGallery = function () {
   setClass(imageContainer, "imgContainer");
   images.forEach((image) => setClass(image, "galleryImage"));
   titles.forEach((title) => setClass(title, "galleryTitle"));
+
   displays.forEach((display, index) => {
     setClass(display, "display");
     display.appendChild(titles[index]);
@@ -276,15 +292,33 @@ const loadGallery = function () {
 
   title1.textContent = "The world's tastiest meal";
   title2.textContent = "Our bestseller";
-  title3.textContent = "Our kitchen";
-  title4.textContent = "Our second";
+  title3.textContent = "Our kitchen - Here we do the magic!";
+  title4.textContent = "Our second kitchen - sneak peek";
 
   content.appendChild(imageContainer);
 };
 
 loadPage();
 
-homeBtn.addEventListener("click", loadPage);
-menuBtn.addEventListener("click", loadMenu);
-aboutBtn.addEventListener("click", loadAbout);
-galleryBtn.addEventListener("click", loadGallery);
+homeBtn.addEventListener("click", () => {
+  loadPage();
+  resetBtnClasses();
+  setClass(homeBtn, "active");
+});
+
+menuBtn.addEventListener("click", () => {
+  loadMenu();
+  resetBtnClasses();
+  setClass(menuBtn, "active");
+});
+
+aboutBtn.addEventListener("click", () => {
+  loadAbout();
+  resetBtnClasses();
+  setClass(aboutBtn, "active");
+});
+galleryBtn.addEventListener("click", () => {
+  loadGallery();
+  resetBtnClasses();
+  setClass(galleryBtn, "active");
+});
